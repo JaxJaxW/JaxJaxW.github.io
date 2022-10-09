1f00f8dbec2acaa832ac92d19a425162b1d10f9c
Jaxton Willman <jaxtonwillman@gmail.com>
2022-10-09 12:55:30 -0500

    IT'S BLUE!
    Changed accent color to profile pic shirt color
    
    Upped my H3 font size and font weight to make them a little more readable
    
    Removed background of resume icon
    
    Added comments in CSS to better separate things and improve readability
    
    Removed main website alert
    
    Added content to UF experience
    
    Removed content from Kyoto experience
    
    Changed research experience to all fall under Saxena Lab
    
    Moved Thermal Lab TA and Peer Advisor experiences to the top of the the engineering experiences list
    
    TODO: Replicate these changes in my resume
    
    Moved projects alert to top of section to explain about project pages and captions
    
    Changed resume download to look more like a button to stand out better
    
    Cleaned up miscellaneous spacing here and there
    
    Changed cwebp.bat tool to not have the duplicated folder structure
    
    TODO: Fix on my desktop folder too
    
    Removed all quotes except Bill Bryson quote
    
    Moved Bill Bryson quote to contact section so it's out of the way a bit
    
    TODO: Fix linkedin profile icon
    TODO: Add background to profile icons
    TODO: Make icons larger and more readable
    
    Thank you to my parents for continually reviewing the content on my website for me and giving me feedback.

--- images/cwebp.bat
+++ images/cwebp.bat
@@ -1,2 +1,2 @@
 @echo on
-for /r %%# in (*.jpg;*.png) do C:\Users\Jaxton\Desktop\libwebp-1.2.4-windows-x64\libwebp-1.2.4-windows-x64\bin\.\cwebp -q 80 /b "%%#" -o "%%~dpn#.webp"
+for /r %%# in (*.jpg;*.png) do C:\Users\Jaxton\Desktop\libwebp-1.2.4-windows-x64\bin\.\cwebp -q 80 /b "%%#" -o "%%~dpn#.webp"
